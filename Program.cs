9f8ee012e951b08158bf0fefba1c1d4bd3673150
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2023-01-15 18:25:52 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else if (n <= 0 || n < 1)
+        Console.WriteLine ("Ошибка!");
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+            return m;
+}
+
+Console.WriteLine(NaturalNumber(n, m));
